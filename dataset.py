2d38cdbe58e79a2c0d663d4700abe7272584c9ba
21ric <55327282+21ric@users.noreply.github.com>
2020-05-19 16:08:04 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,6 +4,28 @@ import torch
 from PIL import Image
 
 class _CIFAR10(CIFAR10):
+  
+  base_folder = 'cifar-10-batches-py'
+    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+    filename = "cifar-10-python.tar.gz"
+    tgz_md5 = 'c58f30108f718f92721af3b95e74349a'
+    train_list = [
+        ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
+        ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
+        ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
+        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
+        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
+    ]
+
+    test_list = [
+        ['test_batch', '40351d587109b95175f43aff81a1287e'],
+    ]
+    meta = {
+        'filename': 'batches.meta',
+        'key': 'label_names',
+        'md5': '5ff9c542aee3614f3951f8cda6e48888',
+    }
+  
   def __init__(self, root, class_range=np.arange(10), train=True, transform=None, target_transform=None, download=False):
     super(_CIFAR10, self).__init__(root, train=train, transform=transform, target_transform=target_transform, download=download)
     
