a90835945f908adc6840a8a7356047142a28634c
21ric <55327282+21ric@users.noreply.github.com>
2020-05-20 13:53:37 +0200

    Update iCaRL.py

--- iCaRL.py
+++ iCaRL.py
@@ -8,9 +8,27 @@ from PIL import Image
 
 from resnet import resnet18
 
+####Hyper-parameters####
+LR = 2
+WEIGHT_DECAY = 0.00001       
+########################
+
 
 class iCarL(nn.Module):
   __init__(self, num_classes):
     super(iCarl,self).__init__()
     self.feature_extractor = resnet18()
+    
+    self.loss = nn.CrossEntropyLoss()
+    self.dist_loss = nn.BCELoss()
+    
+    self.optimizer = optim.SGD(self.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+    self.num_classes = num_classes
+    self.num_know = 0
+ 
+  def forward(self, x):
+    x = self.feature_extractor(x)
+    return(x)
+  
+  
     
