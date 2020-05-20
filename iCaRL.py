2ec56aaa54ba062636d2130b2b5dd55195169424
21ric <55327282+21ric@users.noreply.github.com>
2020-05-20 13:14:15 +0200

    Update iCaRL.py

--- iCaRL.py
+++ iCaRL.py
@@ -1,1 +1,16 @@
-pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+from PIL import Image
+
+from resnet import resnet18
+
+
+class iCarL(nn.Module):
+  __init__(self, num_classes):
+    super(iCarl,self).__init__()
+    self.feature_extractor = resnet18()
+    
