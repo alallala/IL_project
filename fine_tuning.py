f66a26f9307c102d64f055ab3f8c7fc9199f7b09
21ric <55327282+21ric@users.noreply.github.com>
2020-05-20 14:42:14 +0200

    Update fine_tuning.py

--- fine_tuning.py
+++ fine_tuning.py
@@ -119,11 +119,7 @@ def main():
   
   range_classes = np.arange(100)
   classes_groups = np.array_split(range_classes, 10)
-
-  dataset = CIFAR100(root='data/', classes=classes_groups[0], train=True, download=True, transform=train_transform)
-  test_dataset = CIFAR100(root='data/', classes=classes_groups[0],  train=False, download=True, transform=test_transform)
-  train_dataloader = DataLoader(dataset, batch_size=128, shuffle=True, drop_last=True, num_workers=4)  
-  test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=False, drop_last=False, num_workers=4)
+  
 
   #net = resnet18()
   net = resnet32()
