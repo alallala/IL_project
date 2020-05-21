d1b2d6f1aeebe2cf3c0ee3e868ecfb2fd9d4ded2
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-21 16:55:34 +0200

    m1

--- iCaRL.py
+++ iCaRL.py
@@ -55,7 +55,7 @@ def main():
 
         net.reduce_exemplars_set(m)
 
-        for y in range( self.num_known-self.new_classes, self.num_known):
+        for y in range( net.num_classes - net.num_known, net.num_classes):
             net.construct_exemplars_set(train_dataset.get_class_imgs(y), m)
 
 
