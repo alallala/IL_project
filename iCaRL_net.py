f6ab58c9e1b0ac6f3e6b241d56668edf944bae4f
21ric <riccardoluigi.trinchieri@gmail.com>
2020-05-21 13:59:26 +0200

    m1

--- iCaRL_net.py
+++ iCaRL_net.py
@@ -40,7 +40,7 @@ class iCaRL(nn.Module):
 
   def update_representation(self, dataset):
 
-    targets = list(set(self.targets))
+    targets = list(set(datset.targets))
 
     self.to(DEVICE)
     print('{} new classes'.format(len(targets)))
